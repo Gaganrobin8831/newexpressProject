eab2652a11bdd0f1ca0ca97a466072298dc01556
Gaganrobin8831 <gaganrobin71@gmail.com>
2024-09-24 15:03:35 +0530

    manage code properly

--- aggregation/src/controller/order.controller.js
+++ aggregation/src/controller/order.controller.js
@@ -73,9 +73,53 @@ async function HandleGet(req,res) {
         res.status(500).json({ error: err.message });
       }
 }
+async function HandleTodayMonthGet(req,res) {
+  const lastMonth = new Date();
 
+  console.log(lastMonth.getMonth());
+  
+   const newdate = `${lastMonth.getFullYear()}-${lastMonth.getMonth()}-${lastMonth.getDate()}`
+   console.log(new Date(newdate));
+   
+   
+   try {
+     const orders = await Order.aggregate([
+       {
+         $match: {
+           OrderDate: { $gte: new Date(newdate)},
+           totalAmount: { $gte: 100 }
+         }
+       },
+       {
+         $lookup: {
+           from: 'customers',
+           localField: 'customerId',
+           foreignField: '_id',
+           as: 'customerDetails'
+         }
+       },
+       {
+         $unwind: '$customerDetails'
+       },
+       {
+         $project: {
+           orderId: 1,
+           totalAmount: 1,
+           'customerDetails.name': 1,
+           'customerDetails.email': 1
+         }
+       }
+     ]);
+     console.log(orders);
+     
+     res.json(orders);
+   } catch (err) {
+     res.status(500).json({ error: err.message });
+   }
+}
 
 module.exports = {
     HandlePost,
-    HandleGet
+    HandleGet,
+    HandleTodayMonthGet
 }
--- aggregation/src/routes/order.routes.js
+++ aggregation/src/routes/order.routes.js
@@ -1,55 +1,12 @@
 const express = require('express')
-const { HandlePost, HandleGet } = require('../controller/order.controller')
+const { HandlePost, HandleGet, HandleTodayMonthGet } = require('../controller/order.controller')
 const router = express.Router()
 const Order = require('../models/order.models')
 router.route('/')
 .get(HandleGet)
 .post(HandlePost)
 
-router.get('/OrderByDetail',async (req,res)=>{
-    const lastMonth = new Date();
-
-   console.log(lastMonth.getMonth());
-   
-    const newdate = `${lastMonth.getFullYear()}-${lastMonth.getMonth()}-${lastMonth.getDate()}`
-    console.log(new Date(newdate));
-    
-    
-    try {
-      const orders = await Order.aggregate([
-        {
-          $match: {
-            OrderDate: { $gte: new Date(newdate)},
-            totalAmount: { $gte: 100 }
-          }
-        },
-        {
-          $lookup: {
-            from: 'customers',
-            localField: 'customerId',
-            foreignField: '_id',
-            as: 'customerDetails'
-          }
-        },
-        {
-          $unwind: '$customerDetails'
-        },
-        {
-          $project: {
-            orderId: 1,
-            totalAmount: 1,
-            'customerDetails.name': 1,
-            'customerDetails.email': 1
-          }
-        }
-      ]);
-      console.log(orders);
-      
-      res.json(orders);
-    } catch (err) {
-      res.status(500).json({ error: err.message });
-    }
-})
+router.get('/OrderByDetail',HandleTodayMonthGet)
 
 
 
