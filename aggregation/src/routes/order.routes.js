e313cac803a9882d074b0298ad80fc6a3dc04d22
Gaganrobin8831 <gaganrobin71@gmail.com>
2024-09-24 15:05:51 +0530

    spces

--- aggregation/src/routes/order.routes.js
+++ aggregation/src/routes/order.routes.js
@@ -2,13 +2,13 @@ const express = require('express')
 const { HandlePost, HandleGet, HandleTodayMonthGet } = require('../controller/order.controller')
 const router = express.Router()
 const Order = require('../models/order.models')
+
+
 router.route('/')
 .get(HandleGet)
 .post(HandlePost)
 
 router.get('/OrderByDetail',HandleTodayMonthGet)
-
-
 
 
 module.exports = router
