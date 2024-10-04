0d4955d58d8b849a45e828aa299af850864fa8e1
Gaganrobin8831 <gaganrobin71@gmail.com>
2024-10-04 11:11:59 +0530

    do littele changes like minnor

--- chatApp/src/controller/message.controller.js
+++ chatApp/src/controller/message.controller.js
@@ -1,9 +1,10 @@
+// src/controllers/messageController.js
 const Message = require('../models/message.models');
 
 // Get all messages
 const getMessages = async (req, res) => {
   try {
-    const messages = await Message.find();
+    const messages = await Message.find().sort({ timestamp: 1 });
     res.json(messages);
   } catch (error) {
     res.status(500).json({ message: 'Server Error', error });
