f97f29ad65816704b6a2ec553ac042f4dab75134
Gaganrobin8831 <gaganrobin71@gmail.com>
2024-10-11 11:25:22 +0530

    one to one chat is done only some configartion left

--- onetooneChat/src/index.js
+++ onetooneChat/src/index.js
@@ -7,6 +7,7 @@ const { Server } = require('socket.io');
 const io = new Server(http);
 const cookieParser = require('cookie-parser');
 const Message = require('./models/message.models')
+const User = require('./models/user.models')
 
 const path = require('path'); // Import the path module
 const connctDBforChat = require('./DB/database');
@@ -29,21 +30,31 @@ connctDBforChat()
         console.log('A user connected:', socket.id);
     
         // Handle chat message
-        socket.on('chatMessage', async ({ room, message }) => {
-            const msg = new Message({ room, message }); // Save the message with the room info
+        socket.on('chatMessage', async ({message, room,to,from }) => {
+            // console.log({message, room,to,from });
+            const todata = await User.findOne({
+                FullName:to})
+            console.log(todata);
+            
+    // const userId = await User.
+
+            const msg = new Message({ room, content:message,to:todata._id,from}); // Save the message with the room info
             await msg.save(); // Save to database
             io.to(room).emit('recivemessage', message); // Broadcast message to the room
         });
     
         // Handle joining a room
-        socket.on('joinRoom', (room) => {
+        socket.on('joinRoom',async(room) => {
             socket.join(room); // Join the specific room
     
             // Retrieve the chat history from the database for the room
-            Message.find({ room }).then(messages => {
-                const chatHistory = messages.map(msg => msg.message); // Extract messages
-                socket.emit('chatHistory', chatHistory); // Send the chat history to the client
-            });
+           const allMesaageHistoryofusers = await Message.find({ room })
+           console.log(allMesaageHistoryofusers);
+           socket.emit('chatHistory', allMesaageHistoryofusers); 
+        //    allMesaageHistoryofusers.map((messages) => {
+        //         const chatHistory = messages.message // Extract messages
+        //        // Send the chat history to the client
+        //     });
         });
     
         // Handle leaving a room
